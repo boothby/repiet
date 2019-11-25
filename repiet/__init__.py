f42944d2046d364489f9de3218f254c428cba7db
Kelly Boothby <boothby@dwavesys.com>
2019-11-24 21:19:27 -0800

    bumped version

--- repiet/__init__.py
+++ repiet/__init__.py
@@ -5,8 +5,8 @@ from . import tracer
 from . import optimizer
 from . import backends
 __pkgname__ = 'repiet'
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __authorname__ = "Kelly Boothby"
-__authoremail__ = "boothby@github"
+__authoremail__ = ""
 __description__ = "A Piet compiler, targeting a variety of languages (including Piet)"
 __url__ = 'https://github.com/boothby/repiet'
